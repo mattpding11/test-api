d9989d3a3a8bc84a4334d8324a60b931bd8d61f4
Mateo Peñaranda Diaz <mpenarandad@choucairtesting.com>
2025-06-16 07:58:31 -0500

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ export default function App() {
   const agentClient = axios.create({
     baseURL: URL_API_VALIDATOR_AGENT,
     headers: { "Content-Type": "application/json" },
-    timeout: 10_000, // evita cuelgues infinitos
+    timeout: 10000000, // evita cuelgues infinitos
     // validateStatus: () => true,  // descomenta si NO quieres que Axios lance on 4xx/5xx
   });
 
